f6d7727bc19ec3dea61bd06f8a1059ef172476aa
Kadeem140 <Beckford.kadeem@yahoo.com>
2020-04-26 18:55:05 -0400

    Created my reduce & reduxStore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { createStore } from 'redux'
+import { carReducer } from './reducers/carReducer' ; //import my reducer
+ 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+
+const reduxStore = createStore(carReducer); //throw my reducer into the store for access
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
---
+++ src/reducers/carReducer.js
@@ -0,0 +1,30 @@
+export const initialState = {  
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+}
+
+export const carReducer = (state = initialState, action) => {
+    switch(action.type){                     //we are switching data based on the action.type
+        case "ADD_FEATURES":                 //In the case where the type has a value of "ADD_FEATURES"
+            return {                         //Run this code
+                ...state,                    //Make a mutable copy of state
+                    car: { ...state.car,                  
+                        features:[...state.car.features,
+                            state.additionalFeatures[action.payload - 1]     //With this object added to it                   
+                        ]                
+                    },       
+            }
+    }
+}
